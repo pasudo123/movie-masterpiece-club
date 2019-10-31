e64809dac81c7a229fcc9ca690562a853af5805e
PARK SUNG DONG <oraedoa@gmail.com>
2019-10-31 23:57:22 +0900

    [...ing] gradle 멀티 모듈 설정.

--- movie-masterpiece-club-web/src/main/java/com/club/masterpiece/web/aop/UpdatableAspect.java
+++ movie-masterpiece-club-web/src/main/java/com/club/masterpiece/web/aop/UpdatableAspect.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by pasudo123 on 2019-10-30
@@ -32,13 +33,17 @@ public class UpdatableAspect {
 
     private final UpdateStateService updateStateService;
 
+    @Transactional
     @AfterReturning("@annotation(com.club.masterpiece.web.annotation.UpdatableState)")
     public void afterUpdateOnMethod(JoinPoint joinPoint) {
 
         Signature signature = joinPoint.getSignature();
         Class clazz = signature.getDeclaringType();
 
-
+        /**
+         * (1) AOP 따로, Transaction 따로여서 DB 호출이 동기적이지 않다.
+         * (2) OAuth2 유저는 해당 어노테이션을 붙여서 호출되지 않는다.
+         */
         String packageClassName = clazz.getName();
         String className = clazz.getSimpleName();
         String methodName = signature.getName();
