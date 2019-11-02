156f23f03ffc83a7202c8ee2cef969eece0aff91
PARK SUNG DONG <oraedoa@gmail.com>
2019-11-02 16:57:46 +0900

    [update] 그래들 멀티 모듈 세팅.

--- movie-masterpiece-club-common/src/main/java/com/club/masterpiece/common/CommonApplication.java
+++ movie-masterpiece-club-common/src/main/java/com/club/masterpiece/common/CommonApplication.java
@@ -1,13 +1,13 @@
-package com.club.masterpiece.common;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CommonApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CommonApplication.class, args);
-    }
-
-}
+//package com.club.masterpiece.common;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class CommonApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CommonApplication.class, args);
+//    }
+//
+//}
