e14f5c1baedf4b9930df483c4142cfba0b741090
PARK SUNG DONG <oraedoa@gmail.com>
2019-11-02 13:52:40 +0900

    [[...ing] gradle 멀티 모듈 설정..

---
+++ movie-masterpiece-club-admin/src/test/java/com/club/masterpiece/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.masterpiece.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
