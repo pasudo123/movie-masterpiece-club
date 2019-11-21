6e4349dde942b49004eb54f8fd3a37aca8f9f8b3
PARK SUNG DONG <oraedoa@gmail.com>
2019-11-22 00:27:22 +0900

    [ing] gcp 적용 중.

---
+++ movie-masterpiece-club-web/src/main/java/com/club/masterpiece/web/global/infra/GoogleCloudStorageUtil.java
@@ -0,0 +1,20 @@
+package com.club.masterpiece.web.global.infra;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by pasudo123 on 2019-11-18
+ * Email: oraedoa@gmail.com
+ **/
+@Component
+@RequiredArgsConstructor
+public class GoogleCloudStorageUtil {
+
+    public String uploadImage() {
+
+        return null;
+    }
+
+//    public String
+}
