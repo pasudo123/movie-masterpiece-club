047ef5d4c6b83208a97fa50e56a16a714fc9e7be
PARK SUNG DONG <oraedoa@gmail.com>
2019-12-30 10:49:59 +0900

    [refactor] graceful shutdown 주석처리.

--- movie-masterpiece-club-web/src/main/java/com/club/masterpiece/web/WebApplication.java
+++ movie-masterpiece-club-web/src/main/java/com/club/masterpiece/web/WebApplication.java
@@ -1,12 +1,7 @@
 package com.club.masterpiece.web;
 
-import com.club.masterpiece.web.config.GracefulShutdownConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebApplication {
@@ -15,12 +10,12 @@ public class WebApplication {
         SpringApplication.run(WebApplication.class, args);
     }
 
-    @Bean
-    public ConfigurableServletWebServerFactory webServerFactory(final GracefulShutdownConfiguration shutdownConfiguration) {
-
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.addConnectorCustomizers(shutdownConfiguration);
-
-        return factory;
-    }
+//    @Bean
+//    public ConfigurableServletWebServerFactory webServerFactory(final GracefulShutdownConfiguration shutdownConfiguration) {
+//
+//        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+//        factory.addConnectorCustomizers(shutdownConfiguration);
+//
+//        return factory;
+//    }
 }
