eb6de0e506e5db2553506e40436e6dfa36e9661d
PARK SUNG DONG <oraedoa@gmail.com>
2020-01-24 15:21:10 +0900

    [style] 표기 수정.

--- movie-masterpiece-club-web/src/main/java/com/club/masterpiece/web/WebApplication.java
+++ movie-masterpiece-club-web/src/main/java/com/club/masterpiece/web/WebApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class WebApplication {
 
-    private static final String PROPERTIES = "spring.config.addition-location="
+    private static final String PROPERTIES =
+            "spring.config.additional-location="
                     + "/data/etc/gulagbu.com/application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(WebApplication.class)
-                .properties(PROPERTIES);
+                .properties(PROPERTIES).run(args);
     }
 }
