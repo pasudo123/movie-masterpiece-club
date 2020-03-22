4b073df36c2fcfecfb6bdcabe5a286b3a1f89a93
PARK SUNG DONG <oraedoa@gmail.com>
2020-03-23 00:22:11 +0900

    [fix] npm rebuild scss-loader (build.gradle adding npm task)

--- movie-masterpiece-club-web/frontend/vue.config.js
+++ movie-masterpiece-club-web/frontend/vue.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+    return path.join(__dirname, dir)
 }
 
 /** 추후 수행 **/
@@ -10,99 +10,107 @@ function resolve(dir) {
 // const pages = {};
 
 // glob.sync('./src/pages/**/main.js').forEach(path => {
-  // const chunk = path.split('./src/pages/')[1].split('/main.js')[0];
-  // pages[chunk] = {
-  //   entry: path,
-  //   template: 'public/index.html',
-  //   title: chunk,
-  //   chunks: ['chunk-vendors', 'chunk-common', chunk]
-  // };
-  // ./src/pages/article/main.js
-  // ./src/pages/login/main.js
-  // console.debug("============================== path");
-  // console.debug(path);
-  // console.debug("============================== chunk");
-  // console.debug(chunk);
-  // console.debug("============================== pages[chunk]");
-  // console.debug(pages[chunk]);
-  // console.debug("==============================");
+// const chunk = path.split('./src/pages/')[1].split('/main.js')[0];
+// pages[chunk] = {
+//   entry: path,
+//   template: 'public/index.html',
+//   title: chunk,
+//   chunks: ['chunk-vendors', 'chunk-common', chunk]
+// };
+// ./src/pages/article/main.js
+// ./src/pages/login/main.js
+// console.debug("============================== path");
+// console.debug(path);
+// console.debug("============================== chunk");
+// console.debug(chunk);
+// console.debug("============================== pages[chunk]");
+// console.debug(pages[chunk]);
+// console.debug("==============================");
 // });
 
 module.exports = {
-  devServer: {
-    open: true,
-    openPage: 'login',
-    port: 8899,
-    proxy: {
-      [process.env.VUE_APP_BASE_API]: {
-        target: 'http://localhost:8080',
-      },
-      '/*': {
-        target: `http://localhost:8899`,
-        pathRewrite: {'^/.*' : './templates/index.ftl'},
-        ws: false,
-      },
-    }
-  },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.scss$/,
-  //       use: [
-  //         'vue-style-loader',
-  //         'css-loader',
-  //         {
-  //           loader: 'sass-loader',
-  //           options: {
-  //             indentedSyntax: true,
-  //             sassOptions: {
-  //               indentedSyntax: true
-  //             }
-  //           }
-  //         }
-  //       ]
-  //     }
-  //   ],
-  // },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': resolve('src'),
-      },
-      extensions: [
-        '.vue'
-      ]
+    devServer: {
+        open: true,
+        openPage: 'login',
+        port: 8899,
+        proxy: {
+            [process.env.VUE_APP_BASE_API]: {
+                target: 'http://localhost:8080',
+            },
+            '/*': {
+                target: `http://localhost:8899`,
+                pathRewrite: {'^/.*': './templates/index.ftl'},
+                ws: false,
+            },
+        }
     },
-    plugins: [
-      new webpack.ProvidePlugin({
-        'window.Quill': 'quill/dist/quill.js',
-        'Quill': 'quill/dist/quill.js'
-      }),
-    ]
-  },
+    // css: {
+    //     modules: false,
+    //     test: /\.scss$/,
+    //     use: [
+    //         'vue-style-loader',
+    //         'css-loader',
+    //         {
+    //             loader: 'sass-loader',
+    //             options: {
+    //                 indentedSyntax: true,
+    //                 sassOptions: {
+    //                     indentedSyntax: true
+    //                 }
+    //             }
+    //         }
+    //     ]
+    // },
+    chainWebpack(config){
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options => {
+                options.compilerOptions.preserveWhitespace = true;
+                return options;
+            })
+            .end()
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': resolve('src'),
+            },
+            extensions: [
+                '.vue'
+            ]
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                'window.Quill': 'quill/dist/quill.js',
+                'Quill': 'quill/dist/quill.js'
+            }),
+        ]
+    },
 
-  publicPath: (process.env.NODE_ENV === 'staging'
-    || process.env.NODE_ENV === 'production')
-    ? process.env.VUE_APP_BASE_URL
-    : '/',
-  outputDir: '../src/main/resources/static/',
-  assetsDir: '',
-  pages: {
-    index: {
-      entry: 'src/pages/masterpiece/main.js',
-      template: 'public/index.html',
-      title: 'Index',
-      filename: './templates/index.ftl',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-    login: {
-      entry: 'src/pages/login/main.js',
-      template: 'public/index.html',
-      title: 'Login',
-      filename: './templates/login.ftl',
-      chunks: ['chunk-vendors', 'chunk-common', 'login']
+    publicPath: (process.env.NODE_ENV === 'staging'
+        || process.env.NODE_ENV === 'production')
+        ? process.env.VUE_APP_BASE_URL
+        : '/',
+    outputDir: '../src/main/resources/static/',
+    assetsDir: '',
+    pages: {
+        index: {
+            entry: 'src/pages/masterpiece/main.js',
+            template: 'public/index.html',
+            title: 'Index',
+            filename: './templates/index.ftl',
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        login: {
+            entry: 'src/pages/login/main.js',
+            template: 'public/index.html',
+            title: 'Login',
+            filename: './templates/login.ftl',
+            chunks: ['chunk-vendors', 'chunk-common', 'login']
+        }
     }
-  }
     // laboratory: {
     //     entry: './src/pages/laboratory/main.js',
     //     template: 'public/index.html',
@@ -110,5 +118,5 @@ module.exports = {
     //     filename: './templates/laboratory.ftl',
     //     chunks: ['chunk-vendors', 'chunk-common', 'laboratory']
     // }
-  // }
+    // }
 };
